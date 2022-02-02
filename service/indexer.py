e8f002f11f64af305603b52fa3f51d9fa047f9df
thomasmastersmethods <tom.masters@gsscogs.uk>
2022-02-02 09:56:21 +0000

    initial index structure needs to integrate queue and logging

---
+++ service/indexer.py
@@ -0,0 +1,13 @@
+
+def indexer():
+    while True:
+        # TO DO: take items of queue
+        try:
+            pass
+        except Exception as err:
+            # replace with logging system
+            print('following error when taking off queue: %s' % (err))
+
+
+if __name__ == '__main__':
+    indexer()
