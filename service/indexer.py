da624373a4de0999fd2ed1e0625f8eaa39c1d698
thomasmastersmethods <tom.masters@gsscogs.uk>
2022-02-02 16:19:13 +0000

    adding an index worker

--- service/indexer.py
+++ service/indexer.py
@@ -1,15 +1,51 @@
 import sys
 
-def indexer():
+
+class index_worker():
+    """
+        Index worker for queue(TBA) can put/get item on queue
+    """
+
+    def __init__(self, queue):
+        self.queue = queue
+
+    def put_item(self, item):
+        """
+        Put item on queue
+        """
+        return self.queue.put(item)
+
+    def get_item(self):
+        """
+        Get item from queue
+        """
+        return self.queue.get()
+
+    def fetch_index(self, index):
+        """
+        Fetch index of queue
+        """
+        return self.queue[index]
+
+    def drain_queue(self):
+        """
+        Drain queue
+        """
+
+        return self.queue.drain()
+
+
+def indexer(worker):
     while True:
-        # TO DO: take items of queue
+        # TO DO: take items of queue to chosen queue
         try:
-            pass
+            worker.get_item()
         except Exception as err:
             # replace with logging system
             print('following error when taking off queue: %s' % (err))
 
 
 if __name__ == '__main__':
+    worker = index_worker()
     if 'start' in sys.argv:
-        indexer()
+        indexer(worker)
