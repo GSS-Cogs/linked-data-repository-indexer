ad326959c99b2969e18732c498923157842a903f
thomasmastersmethods <tom.masters@gsscogs.uk>
2022-02-17 14:11:44 +0000

    fix logging ini paths

--- service/utils/configuration/log.py
+++ service/utils/configuration/log.py
@@ -1,4 +1,11 @@
+import os
+import sys
 import logging
 from logging.config import fileConfig
-fileConfig('logging.ini')
+
+cur_dir = os.getcwd()
+if 'service' in cur_dir:
+    fileConfig(os.getcwd() + '/logging.ini')
+else:
+    fileConfig(os.getcwd() + '/service/logging.ini')
 logger = logging.getLogger('dev')
